26677364fed1ede6bafc520f5c456068a246c429
root <2053904614@qq.com>
2026-03-29 10:51:24 +0800

    final edition

--- components/main.py
+++ components/main.py
@@ -73,18 +73,18 @@ DATASET_CONFIGS = {
         "device":  "cpu",
     },
     "dgraph": {
-        "min_community_size": 3,
+        "min_community_size": 1,
         "p_bias": 1.0,
-        "r_bias": 1.3,
+        "r_bias": 1.0,
         "grad_norm": 10.0,
-        "target_f1": 0.4,
+        "target_f1": 0.6,
         "stop_reward_scale": 2.0,
         "gamma": 0.99,
         "seedNum": 40,
-        "epoch": 70,  # 大图训练适当减少或保持
+        "epoch": 40,  # 大图训练适当减少或保持
         "f1_base_weight": 1.0,
         "lr": 1e-4,
-        "maxLen": 10,  # 可能需更长路径
+        "maxLen": 2,  # 可能需更长路径
         "hidden_size": 128,
         "device": "cuda" if torch.cuda.is_available() else "cpu",
     },
